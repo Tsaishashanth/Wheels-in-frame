c1759c259f2ed735d3818d071c1a6a4d1e4cf510
Tsaishashanth <saishashanthtarigopula@gmail.com>
2025-08-12 00:46:05 +0530

    added hero section

--- wheelsinframe/src/App.jsx
+++ wheelsinframe/src/App.jsx
@@ -8,23 +8,28 @@ function App() {
 
   return (
     <>
-    <div className="flex flex-row justify-center items-center gap-[30px] h-auto w-auto mt-8">
-  <div className="h-10 w-[10%] bg-black">
-    <img src="wheelsinframe/57006.jpg" alt="Logo" />
-  </div>
 
-  <div className="flex justify-center  h-10 w-[40%] gap-[100px] bg-black text-white">
+    <div className="flex flex-row justify-center items-center gap-[30px] h-auto w-auto ">
+  {/* <div className="h-8 max-w-[5%] bg-black ">
+    <img src="public/57006.jpg" alt="Logo" />
+  </div> */}
+
+  <div className="flex justify-center items-center h-12 w-[40%] gap-[100px] bg-black text-white rounded-[20px]">
     <h1>Home</h1>
     <h1>About Us</h1>
     <h1>Products</h1>
     <h1>Contact</h1>
   </div>
 
-  <div className="flex justify-center items-center h-10 w-[10%] bg-black text-white gap-[20px]">
+  <div className="flex justify-center items-center h-12 w-[10%] bg-black text-white gap-[20px] rounded-[20px]">
     <h1>Login</h1>
     <h1>Signup</h1>
   </div>
 </div>
+
+<section className=" ">
+  <img src="public/heroimage2.png" alt="Wheels in Frame Hero" className=" " />
+</section>
 
     </>
   )
