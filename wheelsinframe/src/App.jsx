4cb252cae3123917bcb51a9b00aa9918629d0f44
Tsaishashanth <saishashanthtarigopula@gmail.com>
2025-08-20 03:02:14 +0530

    basic front page is finished

--- wheelsinframe/src/App.jsx
+++ wheelsinframe/src/App.jsx
@@ -18,7 +18,7 @@ function App() {
 
 
   {/* Navbar */}
-  <div className="absolute top-0 left-0 w-full flex justify-center items-center gap-[100px] p-6">
+  <div className=" fixed absolute top-0 left-0 w-full flex justify-center items-center gap-[100px] p-6">
     {/* Logo Placeholder */}
     {/* <img src="public/logo.png" alt="Logo" className="h-10" /> */}
 
@@ -41,26 +41,50 @@ function App() {
     </div>
   </div>
 </div>
+{/* aboutus section */}
+{/* outercontainer,inner- col,gap */}
+<div className="flex w-full h-full justify-center items-center  bg-black">
+  <div className="flex flex-col p-4 gap-6 text-center mt-8 mb-8">
+    <div className="font-bricolage text-white text-4xl">
+      Wheels In Frame
+    </div>
+    <div className="text-gray-400 font-light uppercase text-2xl">
+      We’re not just another shopping site, we’re a smart comparison hub for bikers!
+    </div>
+    <div className ="text-gray-400 font-light text-xl">Wheels in Frame helps riders save time, save money, and focus on what truly matters.</div>
+  </div>
+</div>
+
+
 {/* menu section */}
 <div className="flex items-center justify-center  w-full h-auto gap-[20px] mt-8">
-  <div className="flex flex-col items-center justify-center w-[30%] p-4 gap-[12px]">
+  <div className="flex flex-col items-center justify-center w-[30%] p-4 gap-[12px] transition duration-300 hover:shadow-[0_0] hover:scale-105">
     <img src="/helmet_image.png" className="w-[100%] rounded-[20px]"/>
     <h1 className="font-light uppercase tracking-[2px] text-sm text-gray-400">Helmets</h1>
   </div>
-  <div className="flex flex-col items-center justify-center w-[30%] p-4 gap-[12px]">
+  <div className="flex flex-col items-center justify-center w-[30%] p-4 gap-[12px] tansition duration-300 hover:shadow-[0] hover:scale-105">
     <img src="/riddingjacket_image.png" className="w-[100%] rounded-[20px]"/>
     <h1 className="font-light uppercase tracking-[2px] text-sm text-gray-400"> jackets</h1>
   </div>
-  <div className="flex flex-col items-center justify-center w-[30%] p-4 gap-[12px]">
+  <div className="flex flex-col items-center justify-center w-[30%] p-4 gap-[12px] transition duration-300 hover:shadow-[0] hover:scale-105">
     <img src="/riddinggloves_img.png" className="w-[100%] rounded-[20px]"/>
     <h1 className="font-light uppercase tracking-[2px] text-sm text-gray-400"> gloves</h1>
   </div>
-  <div className="flex flex-col items-center justify-center w-[30%] p-4 gap-[12px]">
+  <div className="flex flex-col items-center justify-center w-[30%] p-4 gap-[12px] transition duration-300 hover:shadow-[0] hover:scale-105">
     <img src="/riddingshoes_img.png" className="w-[100%] rounded-[20px]"/>
     <h1 className="font-light uppercase tracking-[2px] text-sm text-gray-400"> shoes</h1>
   </div>
 </div>
 
+
+<footer className="bg-gray-900 text-gray-400 py-4 text-center mt-8  ">
+  <p className="text-xl mt-1">
+    © {new Date().getFullYear()} Wheels In Frame. All rights reserved.
+  </p>
+  <p className="text-l mt-1 mt-4 mb-2">
+    Built for bikers by Tarigopula Sai Shashanth
+  </p>
+</footer>
     </>
   )
 }
